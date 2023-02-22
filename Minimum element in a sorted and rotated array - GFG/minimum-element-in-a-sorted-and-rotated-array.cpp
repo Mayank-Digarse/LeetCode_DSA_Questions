fcd5c033e18038450b2091938b6a58197f489954
Mayankd21 <110493499+Mayank-Digarse@users.noreply.github.com>
2023-02-22 22:50:22 +0530

    Added solution - LeetHub

--- Minimum element in a sorted and rotated array - GFG/minimum-element-in-a-sorted-and-rotated-array.cpp
+++ Minimum element in a sorted and rotated array - GFG/minimum-element-in-a-sorted-and-rotated-array.cpp
@@ -7,8 +7,27 @@ class Solution{
 public:
     int findMin(int arr[], int n){
         //complete the function here
-        sort(arr , arr+n);
-        return arr[0];
+        int s = 0;
+        int e = n-1;
+        int mid ;
+        while(s<=e){
+            mid = s+(e-s)/2;
+            if(arr[s] <= arr[e]){
+                return arr[s];
+            }
+            if(arr[mid-1] < arr[mid] && arr[mid] > arr[mid+1]){
+                return arr[mid+1];
+            }
+            else if(arr[mid] <arr[mid-1] && arr[mid] <arr[mid+1]){
+                return arr[mid];
+            }
+            else if(arr[mid] >arr[s]){
+                s = mid+1;
+            }
+            else{
+                e = mid-1;
+            }
+        }
     }
 };
 
